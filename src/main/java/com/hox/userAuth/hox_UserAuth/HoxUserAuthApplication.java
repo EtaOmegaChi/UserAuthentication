e23601405314fbb8f5c7f3ba14768795c01d2906
Rui Ji <ruiji@usc.edu>
2023-02-28 13:16:38 -0800

    first commit

---
+++ src/main/java/com/hox/userAuth/hox_UserAuth/HoxUserAuthApplication.java
@@ -0,0 +1,13 @@
+package com.hox.userAuth.hox_UserAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoxUserAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoxUserAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hox/userAuth/hox_UserAuth/HoxUserAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hox.userAuth.hox_UserAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoxUserAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
